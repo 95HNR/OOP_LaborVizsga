46bc475358d1d84622eeabd2bb4206e3e2113d6d
95HNR <megnemmondodki@gmail.com>
2025-11-03 09:53:46 +0200

    Task01
    data.json parse-olasa

--- Car-Service-Intake/src/main/java/com/mycompany/car/service/intake/CarServiceIntake.java
+++ Car-Service-Intake/src/main/java/com/mycompany/car/service/intake/CarServiceIntake.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.oop_laborvizsga;
+package com.mycompany.car.service.intake;
 
 /**
  *
  * @author Hunor
  */
-public class OOP_LaborVizsga {
+public class CarServiceIntake {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
